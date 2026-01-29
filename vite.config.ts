ebacd173f6046be9f8a6d7484b1939964d360bec
Rik de Wit <rikdewit1997@gmail.com>
2026-01-29 02:09:47 +0100

    Change base path in Vite config from './' to '/'

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 // Vervang 'REPOSNAAM' door de naam van je github repository als het geen custom domein is
 export default defineConfig({
   plugins: [react()],
-  base: './', 
+  base: '/', 
   build: {
     outDir: 'dist',
   }
